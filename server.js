7f37980912c120a818729b199557593dd9372638
Justin Pickens <justin.pickens@gmx.com>
2021-07-17 04:43:23 -0500

    Added GET routes

--- server.js
+++ server.js
@@ -16,5 +16,10 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 //      Get Notebook
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
+//      Get Assets
+app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, './public/assets/css/styles.css')));
+app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, './public/assets/js/index.js')));
+app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+
 // Listen
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
