9f97ae9d3396e4d17fee0b8f60a98bf220e8ef17
Justin Pickens <justin.pickens@gmx.com>
2021-07-21 00:26:24 -0500

    Working POST route, and now clicking on a previous note correctly displays it to the screen

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //      Imports
 const express = require('express');
 const path = require('path');
+const File = require('fs');
 
 //      Express Config
 const app = express();
@@ -20,6 +21,24 @@ app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/note
 app.get('/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, './public/assets/css/styles.css')));
 app.get('/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, './public/assets/js/index.js')));
 app.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, './db/db.json')));
+
+//      Post to db
+app.post('/api/notes', (req, res) => {
+
+    let container; // Container variable for data operation
+    let dbPath = path.join(__dirname, './db/db.json');
+
+    File.readFile(dbPath, 'utf8', (err, data) => {
+
+        if (err) console.error('ERROR: Could not read file!');
+
+        container = JSON.parse(data);
+        container.push(req.body);
+
+        File.writeFile(dbPath, JSON.stringify(container), (err) => {});
+    })
+
+});
 
 // Listen
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
